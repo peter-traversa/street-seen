1b6b032eaebb883ab67aa3df51ba25767ed34ae9
github username <github email address>
2018-10-10 17:02:38 -0400

    placeholder components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import MapComponent from './components/MapComponent'
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  handleButtonClick = () => {
+    console.log('main button was clicked')
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Button onClick={ this.handleButtonClick } >A Button!</Button>
+        <MapComponent />
       </div>
     );
   }
---
+++ src/components/MapComponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class MapComponent extends Component {
+  render() {
+    return <h1>This is where a map is going to go!</h1>
+  }
+}
